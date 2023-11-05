852df613311a752c7b624c3998a63c201fc1f758
Your Name <you@example.com>
2023-11-05 12:55:09 +0700

    finish things

---
+++ Core/Inc/input_processing.h
@@ -0,0 +1,14 @@
+/*
+ * input_processing.h
+ *
+ *  Created on: Nov 1, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_INPUT_PROCESSING_H_
+#define INC_INPUT_PROCESSING_H_
+
+void fsm_for_mode_processing(void);
+void fsm_for_button_processing(void);
+
+#endif /* INC_INPUT_PROCESSING_H_ */
